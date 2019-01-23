7af28c01906bad5ff8737fbe6d5c3bf4129f4b9f
Dima Chekurkov <Chekurkov@gmail.com>
2019-01-23 22:59:36 +0200

    FixBags

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
-/* GAME */
 
-/* array of words*/
-var words = ["javascript", "pancake", "monkey"];
-/* random word*/
-var word = words[Math.floor(Math.random() * words.length)];
-/* answer */
+var words = ["javascript","esmascript","java"];
+var word = words[Math.floor(Math.random()* words.length)];
+
 var answerArray = [];
 
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
-var remainingLetters = word.length;
 
-while (remainingLetters > 0) {
+var remainingLetters = word.length;
+var guesses = 10;
+
+while (remainingLetters > 0 && guesses > 0 ) {
     alert(answerArray.join(" "));
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    if (guess === null){
+
+    var guess = prompt("Guess a letter, or click cancel to stop game");
+    if (guess === null) {
         break;
-    }else if (guess.length !== 1) {
-        alert("Pleace enter a single letter");
-    }else {
+    } else if (guess.length !== 1){
+        alert("Enter a single letter");
+    }else{
+        guesses--;
+        guess = guess.toLocaleLowerCase();
         for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess){
+            if (word[j] == guess && answerArray[j] === "_") {
                 answerArray[j] = guess;
                 remainingLetters--;
             }
         }
     }
 }
-alert(answerArray.join(" "));
-alert("Good job! The answer was " + word);
 
+alert(answerArray.join(" "));
+if (guesses > 0) {
+    alert("Отлично! Было загадано слово " + word);
+} else {
+    alert("Очень плохо! Было загадано слово " + word);
+}
